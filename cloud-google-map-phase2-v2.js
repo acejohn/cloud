03a567eb93676db1170983f1f77c60fd6daf83cd
avilaacejohn <avilaacejohn@gmail.com>
2019-10-24 13:08:56 +0800

    Filter marker with NULL values

--- cloud-google-map-phase2-v2.js
+++ cloud-google-map-phase2-v2.js
@@ -14,130 +14,134 @@
     
     
 var map;
-var markers = [];
-var InforObj = [];
-var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var markerCluster;
+    var markers = [];
+    var InforObj = [];
+    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var markerCluster;
+    
+        function initMap() {
 
-    function initMap() {
+            var infowindow = new google.maps.InfoWindow();
+            var bounds = new google.maps.LatLngBounds();
 
-        var infowindow = new google.maps.InfoWindow();
-        var bounds = new google.maps.LatLngBounds();
+            var haightAshbury = { lat: 37.769, lng: -122.446 };
 
-        var haightAshbury = { lat: 37.769, lng: -122.446 };
+            map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 12,
+                center: haightAshbury,
+                mapTypeId: 'terrain'
+            });
 
-        map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 12,
-            center: haightAshbury,
-            mapTypeId: 'terrain'
-        });
+            var oms = new OverlappingMarkerSpiderfier(map, {
+                markersWontMove: true,
+                markersWontHide: true,
+                basicFormatEvents: true
+            });
 
-        var oms = new OverlappingMarkerSpiderfier(map, {
-            markersWontMove: true,
-            markersWontHide: true,
-            basicFormatEvents: true
-        });
+            function reload() { 
+                 $.ajax({
+                        url: '@Url.Action("GetAllDeviceByAccount", "Device", new { Area = "AccountAdmin" })',
+                        type: "GET",
+                        data: {},
+                         success: function (data) {
+                             console.log(data)
 
-        function reload() { 
-             $.ajax({
-                    url: '@Url.Action("GetAllDeviceByAccount", "Device", new { Area = "AccountAdmin" })',
-                    type: "GET",
-                    data: {},
-                     success: function (data) {
-                         console.log(data)
+                             var t = data;
+                             
 
-                         var t = data;
-                         //heartbeat val here
-                        var status = true;
+                             $.each(t, function (marker, t) {
 
-                        
+                                 if (t.Latitude !== null && t.Longitude !== null) {
 
-                        if (status == true) {
-                            var blink = '<i class="fa fa-circle active Blink-mapStat"></i><span style="margin-left: 6px;">Active</span> <hr>';
-                        } else {
-                            var blink = '<i class="fa fa-circle inactive" style="color: #9E9E9E"></i><span style="color:#9E9E9E;margin-left: 6px;">Inactive</span><hr>';
-                        }
+                                 var DevStatus = t.IsOnline;
 
-                         $.each(t, function (marker, t) {
+                                 console.log(DevStatus)
 
-                             var defaultLink = "@Url.Action("GetConfiguration", "Configuration", new { Area = "AccountAdmin", clientId = Session["CurrentClientId"], accountId = "/", deviceId = "/", deviceTypeName = "/" })";
-                             var deviceUrl = t.AccountId + "/" + t.DeviceId + "/" + t.DeviceTypeName;
-
-                             var contentString = '<span><a id="configureBtn" onclick="test()" href=" ' + defaultLink + deviceUrl + '" data-toggle="tooltip" data-placement="top" title="Go to device configuration"><img class="device-img" src=" ' + t.DevicePic + ' "></a></span>' + blink +
-                                 '<span>Country: </span>' + t.Country +
-                                 '<br/><span>Device Type: </span>' + t.DeviceTypeName +
-                                 '<br/><span>Device Name: </span>' + t.DeviceName;
-
-                             var expectedPosition = new google.maps.LatLng(t.Latitude, t.Longitude);
-
-                             var marker = new google.maps.Marker({
-                                 position: expectedPosition,
-                                 label: labels[t % labels.length],
-                                 map: map
-                             });
-
-                             bounds.extend(marker.position);
-                             map.fitBounds(bounds);
-                             markers.push(marker);
-                             oms.addMarker(marker);                     
-
-                             const infowindow = new google.maps.InfoWindow({
-                                 content: contentString,
-                                 maxWidth: 200
-                             });
-
-                             google.maps.event.addListener(marker, 'mouseover', (function (marker) {
-                                 return function () {
-                                     closeOtherInfo();
-                                     infowindow.open(map, marker);
-                                     InforObj[0] = infowindow;
+                                 if (DevStatus == true) {
+                                     var blink = '<i class="fa fa-circle active Blink-mapStat"></i><span style="margin-left: 6px;">Online</span> <hr>';
+                                 } else {
+                                     var blink = '<i class="fa fa-circle inactive" style="color: #9E9E9E"></i><span style="color:#9E9E9E;margin-left: 6px;">Offline</span><hr>';
                                  }
-                             })(marker, t));                                               
-                         });
-                         markerCluster = new MarkerClusterer(map, markers,
-                             {
-                                 imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
-                                 //gridSize: 100,
-                                 zoomOnClick: true,
-                                 maxZoom: 10
-                             }); 
-                 
-                    },
-                    error: function (error) { }
-                });
-            }
-         
-            setInterval(function () {
-                deleteMarkers();
+
+                                 var defaultLink = "@Url.Action("GetConfiguration", "Configuration", new { Area = "AccountAdmin", clientId = Session["CurrentClientId"], accountId = "/", deviceId = "/", deviceTypeName = "/" })";
+                                 var deviceUrl = t.AccountId + "/" + t.DeviceId + "/" + t.DeviceTypeName;
+
+                                 var contentString = '<span><a id="configureBtn" onclick="test()" href=" ' + defaultLink + deviceUrl + '" data-toggle="tooltip" data-placement="top" title="Go to device configuration"><img class="device-img" src=" ' + t.DevicePic + ' "></a></span>' + blink +
+                                     '<span>Country: </span>' + t.Country +
+                                     '<br/><span>Device Type: </span>' + t.DeviceTypeName +
+                                     '<br/><span>Device Name: </span>' + t.DeviceName;
+
+                                 var expectedPosition = new google.maps.LatLng(t.Latitude, t.Longitude);
+
+                                 
+                                     var marker = new google.maps.Marker({
+                                         position: expectedPosition,
+                                         label: labels[t % labels.length],
+                                         map: map
+                                     });
+                                     bounds.extend(marker.position);
+                                     map.fitBounds(bounds);
+                                     markers.push(marker);
+                                     oms.addMarker(marker);
+                                                                  
+                                     const infowindow = new google.maps.InfoWindow({
+                                         content: contentString,
+                                         maxWidth: 200
+                                     });
+
+                                 google.maps.event.addListener(marker, 'mouseover', (function (marker) {
+                                     return function () {
+                                         closeOtherInfo();
+                                         infowindow.open(map, marker);
+                                         InforObj[0] = infowindow;
+                                     }
+                                 })(marker, t));
+                             }
+                             });
+                                markerCluster = new MarkerClusterer(map, markers,
+                                 {
+                                     imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
+                                     //gridSize: 100,
+                                     zoomOnClick: true,
+                                     maxZoom: 10
+                                 }); 
+                     
+                        },
+                        error: function (error) { }
+                    });
+                }
+             
+                setInterval(function () {
+                    deleteMarkers();
+                    reload();
+                }, 30000);
                 reload();
-            }, 30000);
-            reload();
-    }
-
-    function setMapOnAll(map) {
-        for (var i = 0; i < markers.length; i++) {
-            markers[i].setMap(map);
-            markerCluster.removeMarker(markers[i]);
         }
-    }
 
-    function clearMarkers() {
-        setMapOnAll(null);      
-    }
-
-    function showMarkers() {
-        setMapOnAll(map);
-    }
-
-    function deleteMarkers() {
-        clearMarkers();
-        markers = [];
-    }
-
-    function closeOtherInfo() {
-        if (InforObj.length > 0) {
-            InforObj[0].set("marker", null);
-            InforObj[0].close();
-            InforObj.length = 0;
+        function setMapOnAll(map) {
+            for (var i = 0; i < markers.length; i++) {
+                markers[i].setMap(map);
+                markerCluster.removeMarker(markers[i]);
+            }
         }
-    }
+
+        function clearMarkers() {
+            setMapOnAll(null);      
+        }
+
+        function showMarkers() {
+            setMapOnAll(map);
+        }
+
+        function deleteMarkers() {
+            clearMarkers();
+            markers = [];
+        }
+
+        function closeOtherInfo() {
+            if (InforObj.length > 0) {
+                InforObj[0].set("marker", null);
+                InforObj[0].close();
+                InforObj.length = 0;
+            }
+        }
